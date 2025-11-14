fee9661a87bb85ab497252b6951bfdb00e64fb5d
Evgeniy Margovich <evmargovich@gmail.com>
2025-11-14 20:46:21 +0200

    HW 14.11.2025

--- library_function.js
+++ library_function.js
@@ -1,5 +1,3 @@
-//import { faker } from 'https://esm.sh/@faker-js/faker';
-
 
 function Book(isbn, title, author, year) {
     this.isbn = isbn;
@@ -14,54 +12,36 @@ Book.prototype.toString = function(){
 
 function Library() {
     this.books = [];
-   // this.isbns = new Set();
 }
 
-// Library.prototype.addBook = function(book){
-//     this.books.push(book);
+// Library.prototype.findByAuthor = function(author){
+//     author = author.toLowerCase();
+//     return this.books.filter(b => b.author.toLowerCase().includes(author));
+// }
+//
+// Library.prototype.findByTitle = function(title){
+//     title = title.toLowerCase();
+//     return this.books.filter(b => b.title.toLowerCase().includes(title) );
 // }
 
-// Library.prototype.generateRandomBooks = function(count, minYear, maxYear) {
-//     for(let i = 0; i < count; i++){
-//     const isbn = this.generateUniqueISBN();
-//     const authorNumber = getRandomInt(1, 30);
-//     const author = `Author${authorNumber}`;
-//     const titleNumber = getRandomInt(1, 100);
-//     const title = `Title${titleNumber}`;
-//     const year = getRandomInt(minYear, maxYear);
-//     const book = new Book(isbn, title, author, year);
-//     this.books.push(book);
-// }}
+Library.prototype.findByAuthorTitle = function(author, title){
+    author = author.toLowerCase().trim();
+    console.log(author);
+    title = title.toLowerCase().trim();
+    console.log(title);
 
-Library.prototype.findByAuthor = function(author){
-    author = author.toLowerCase();
-    return this.books.filter(b => b.author.toLowerCase().includes(author));
-}
-
-Library.prototype.findByTitle = function(title){
-    title = title.toLowerCase();
-    return this.books.filter(b => b.title.toLowerCase().includes(title) );
+    return this.books.filter(book => {
+        // const res = (author === "" ? true : book.author.toLowerCase().includes(author)) &&
+        // (title === "" ? true : book.title.toLowerCase().includes(title));
+        //console.log(res);
+        return (author === "" ? true : book.author.toLowerCase().includes(author)) &&
+            (title === "" ? true : book.title.toLowerCase().includes(title));
+    } );
 }
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-// Library.prototype.generateUniqueISBN = function() {
-//         let isbn;
-//         do{
-//             isbn = this.generateRandomISBN();
-//         } while(this.isbns.has(isbn));
-//         return isbn;
-//     }
-//
-// Library.prototype.generateRandomISBN = function() {
-//         let isbn= "";
-//         for(let i = 0; i < 10; i++) {
-//             isbn += Math.floor(Math.random() * 10);
-//         }
-//         return isbn;
-//     }
 
 Library.prototype.printLibrary = function(arrBooks) {
     let strBooks = "";
@@ -75,7 +55,7 @@ Library.prototype.generateRandomBooks2 = function (count, minYear, maxYear) {
     for(let i = 0; i < count; i++){
         let isbn = faker.commerce.isbn()
         let year = getRandomInt(minYear, maxYear);
-        let author = faker.book.author();
+        let author = faker.person.fullName();
         let title = faker.book.title();
         const book = new Book(isbn, title, author, year);
         this.books.push(book);
@@ -85,8 +65,8 @@ Library.prototype.generateRandomBooks2 = function (count, minYear, maxYear) {
 const library  = new Library();
 
 const createLibraryButton = document.querySelector("#createLibraryButton");
-const searchByAuthorButton = document.querySelector("#searchByAuthorButton");
-const searchByTitleButton = document.querySelector("#searchByTitleButton");
+//const searchByAuthorButton = document.querySelector("#searchByAuthorButton");
+//const searchByTitleButton = document.querySelector("#searchByTitleButton");
 const numBooks = document.querySelector("#inNumBooks");
 const minYear = document.querySelector("#minYear");
 const maxYear = document.querySelector("#maxYear");
@@ -94,31 +74,40 @@ const authorForSearch = document.querySelector("#inputSearchByAuthor");
 const titleForSearch = document.querySelector("#inputSearchByTitle");
 const searchRes = document.querySelector("#searchResult");
 
-
 createLibraryButton.addEventListener("click", function(){
     const min = Number(minYear.value);
     library.generateRandomBooks2(numBooks.value, min, maxYear.value);
     searchRes.textContent = library.printLibrary(library.books);
 });
 
-searchByAuthorButton.addEventListener("click", function(){
-    findByAuthorByEvent();
-});
+// searchByAuthorButton.addEventListener("click", function(){
+//     findByAuthorByEvent();
+// });
 
 authorForSearch.addEventListener("keyup", function(event) {
-    findByAuthorByEvent()
+    findByEvent();
 });
 
-let findByAuthorByEvent = function (){
-    searchRes.textContent = "";
-    titleForSearch.value = "";
-    searchRes.textContent = library.printLibrary(library.findByAuthor(authorForSearch.value));
-};
+titleForSearch.addEventListener("keyup", function(event) {
+    findByEvent();
+})
 
-searchByTitleButton.addEventListener("click", function(){
+// let findByAuthorByEvent = function (){
+//     searchRes.textContent = "";
+//     titleForSearch.value = "";
+//     searchRes.textContent = library.printLibrary(library.findByAuthor(authorForSearch.value, ""));
+// };
+
+let findByEvent = function (){
     searchRes.textContent = "";
-    searchRes.textContent = library.printLibrary(library.findByTitle(titleForSearch.value));
-});
+    searchRes.textContent = library.printLibrary(library.findByAuthorTitle(authorForSearch.value, titleForSearch.value));
+
+}
+
+// searchByTitleButton.addEventListener("click", function(){
+//     searchRes.textContent = "";
+//     searchRes.textContent = library.printLibrary(library.findByTitle(titleForSearch.value));
+// });
 
 
 
