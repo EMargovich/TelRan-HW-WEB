6301b793fadf670eeda33ca9b9157cd72e7dc835
Evgeniy Margovich <evmargovich@gmail.com>
2025-11-23 08:29:34 +0200

    HW 23.11.2025

--- lost_context.js
+++ lost_context.js
@@ -19,102 +19,9 @@
 // kir.infoFunction.call({ nickName: "Kir" }, "Mrs", "!");
 // kir.infoFunction.apply({ nickName: "Kir" }, ["Mrs", "!"]);
 
+debugger;
 let group = {
     title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = function (name){
-            //debugger;
-            console.log(`${this.title}: ${name}`)
-        }
-        this.students.forEach(show)
-    }
-};
-
-group.showList();
-
-console.log('First try: use bind()');
-
-let group1 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = function (name){
-            console.log(`${this.title}: ${name}`)
-        }
-        this.students.forEach(show.bind(this));
-    }
-};
-
-group1.showList();
-
-console.log('Second try: set parameter with link to this in outer function');
-
-let group2 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function (t = this.title) {
-        const show = function (name){
-            console.log(`${t}: ${name}`)
-        }
-        this.students.forEach(show);
-    }
-};
-
-group2.showList();
-
-console.log('Third try: second parameter in inner function');
-
-let group3 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = function (name, t){
-            console.log(`${t}: ${name}`)
-        }
-        this.students.forEach((name) => show(name, this.title));
-    }
-};
-
-group3.showList();
-
-console.log('Four try: use const instead of function in inner function');
-
-let group4 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = (name) => {
-            console.log(`${this.title}: ${name}`)
-        }
-        this.students.forEach(show)
-    }
-};
-
-group4.showList();
-
-console.log('Five try: save this in const in outer function and use this constant in inner function === second try');
-
-let group5 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const newThis = this;
-        const show = function (name){
-            console.log(`${newThis.title}: ${name}`)
-        }
-        this.students.forEach(show)
-    }
-};
-
-group5.showList();
-
-console.log("6: set global parameter")
-
-title = "Carmiel 2025 - 2026";
-
-let group6 = {
-    //title: "Carmiel 2025 - 2026",
     students: ['Kir', 'Masha', 'Oleg'],
     showList: function () {
         const show = function (name){
@@ -125,82 +32,176 @@ let group6 = {
     }
 };
 
-group6.showList();
-
-title = undefined;
-console.log("7: use apply")
-
-let group7 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = function (name){
-            console.log(`${this.title}: ${name}`)
-        }
-        //this.students.forEach(st => show.apply({title: "Carmiel 2025 - 2026"}, [st]))
-        this.students.forEach(st => show.apply(this, [st]))
-    }
-};
-
-group7.showList();
-
-console.log("8: use call")
-
-let group8 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = function (name){
-            console.log(`${this.title}: ${name}`)
-        }
-        //this.students.forEach(st => show.apply({title: "Carmiel 2025 - 2026"}, [st]));
-        this.students.forEach(st => show.call(this, st));
-    }
-};
-
-group8.showList();
-
-console.log('9: call group.title parameter')
-
-let group9 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = function (name){
-            console.log(`${group.title}: ${name}`)
-        }
-        this.students.forEach(show)
-    }
-};
-
-group9.showList();
-
-console.log('10: use forEach parameters thisArg')
-
-let group10 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = function (name){
-            console.log(`${this.title}: ${name}`)
-        }
-        this.students.forEach(show, this);
-    }
-};
-
-group10.showList();
-
-console.log('11: use bind when describe the function')
-
-let group11 = {
-    title: "Carmiel 2025 - 2026",
-    students: ['Kir', 'Masha', 'Oleg'],
-    showList: function () {
-        const show = function (name){
-            console.log(`${this.title}: ${name}`)
-        }.bind(this);
-        this.students.forEach(show);
-    }
-};
-
+// group.showList();
+//
+// console.log('First try: use bind()');
+//
+// let group1 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = function (name){
+//             console.log(`${this.title}: ${name}`)
+//         }
+//         this.students.forEach(show.bind(this));
+//     }
+// };
+//
+// group1.showList();
+//
+// console.log('Second try: set parameter with link to this in outer function');
+//
+// let group2 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function (t = this.title) {
+//         const show = function (name){
+//             console.log(`${t}: ${name}`)
+//         }
+//         this.students.forEach(show);
+//     }
+// };
+//
+// group2.showList();
+//
+// console.log('Third try: second parameter in inner function');
+//
+// let group3 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = function (name, t){
+//             console.log(`${t}: ${name}`)
+//         }
+//         this.students.forEach((name) => show(name, this.title));
+//     }
+// };
+//
+// group3.showList();
+//
+// console.log('Four try: use const instead of function in inner function');
+//
+// let group4 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = (name) => {
+//             console.log(`${this.title}: ${name}`)
+//         }
+//         this.students.forEach(show)
+//     }
+// };
+//
+// group4.showList();
+//
+// console.log('Five try: save this in const in outer function and use this constant in inner function === second try');
+//
+// let group5 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const newThis = this;
+//         const show = function (name){
+//             console.log(`${newThis.title}: ${name}`)
+//         }
+//         this.students.forEach(show)
+//     }
+// };
+//
+// group5.showList();
+//
+// console.log("6: set global parameter")
+//
+// title = "Carmiel 2025 - 2026";
+//
+// let group6 = {
+//     //title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = function (name){
+//             debugger;
+//             console.log(`${this.title}: ${name}`)
+//         }
+//         this.students.forEach(show)
+//     }
+// };
+//
+// group6.showList();
+//
+// title = undefined;
+// console.log("7: use apply")
+//
+// let group7 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = function (name){
+//             console.log(`${this.title}: ${name}`)
+//         }
+//         //this.students.forEach(st => show.apply({title: "Carmiel 2025 - 2026"}, [st]))
+//         this.students.forEach(st => show.apply(this, [st]))
+//     }
+// };
+//
+// group7.showList();
+//
+// console.log("8: use call")
+//
+// let group8 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = function (name){
+//             console.log(`${this.title}: ${name}`)
+//         }
+//         //this.students.forEach(st => show.apply({title: "Carmiel 2025 - 2026"}, [st]));
+//         this.students.forEach(st => show.call(this, st));
+//     }
+// };
+//
+// group8.showList();
+//
+// console.log('9: call group.title parameter')
+//
+// let group9 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = function (name){
+//             console.log(`${group.title}: ${name}`)
+//         }
+//         this.students.forEach(show)
+//     }
+// };
+//
+// group9.showList();
+//
+// console.log('10: use forEach parameters thisArg')
+//
+// let group10 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = function (name){
+//             console.log(`${this.title}: ${name}`)
+//         }
+//         this.students.forEach(show, this);
+//     }
+// };
+//
+// group10.showList();
+//
+// console.log('11: use bind when describe the function')
+//
+// let group11 = {
+//     title: "Carmiel 2025 - 2026",
+//     students: ['Kir', 'Masha', 'Oleg'],
+//     showList: function () {
+//         const show = function (name){
+//             console.log(`${this.title}: ${name}`)
+//         }.bind(this);
+//         this.students.forEach(show);
+//     }
+// };
+//
 group11.showList();
---
+++ nasa/nasa.js
@@ -0,0 +1,129 @@
+const BASE_URL = 'https://api.nasa.gov/planetary/apod';
+const API_KEY = 'dQfwmNYPWTKcqhIoFgdLHrzZDbdwgWeyypdjzkEh'
+const inputOneDate = document.getElementById('oneDate');
+const inputFirstDate = document.getElementById('twoDate1');
+const inputSecondDate = document.getElementById('twoDate2');
+const inputCount = document.getElementById('count');
+let currentDate = new Date().toISOString().split('T')[0];
+
+//Set count borders
+inputCount.min = 1;
+inputCount.max = 10;
+
+inputCount.addEventListener('change', () => {
+    if(inputCount.value < 1) inputCount.value = 1;
+    if(inputCount.value > 10) inputCount.value = 10;
+});
+
+//Set date borders
+const MIN_DATE = "1996-06-16";
+inputOneDate.max = currentDate;
+inputOneDate.min = MIN_DATE;
+inputFirstDate.max = currentDate;
+inputFirstDate.min = MIN_DATE;
+inputSecondDate.max = currentDate;
+inputSecondDate.min = MIN_DATE;
+
+//Check first and second dates
+inputFirstDate.addEventListener('change', () => {
+    inputSecondDate.min = inputFirstDate.value;
+    if (inputSecondDate.min > inputSecondDate.value)
+        inputSecondDate.value =  inputSecondDate.min;
+    inputFirstDate.style.border = '';
+});
+
+function displayResult(data, container) {
+    const wrap = document.createElement('div');
+    wrap.style.marginBotton = '20px';
+    const h3 = document.createElement('h3');
+    wrap.appendChild(h3);
+    if (data.media_type === 'image') {
+        const img = document.createElement('img');
+        img.src = data.hdurl||data.url;
+        img.alt = data.title||"APOD image";
+        img.style.maxWidth = '800px';
+        img.style.maxHeight = '600px';
+        img.style.width = '100%';
+        img.style.height= 'auto';
+        wrap.appendChild(img);
+    } else if (data.media_type === 'video') {
+        const iframe = document.createElement('iframe');
+        iframe.src = data.url;
+        iframe.width = '560';
+        iframe.height = '315';
+        iframe.allowFullscreen = true;
+        iframe.loading = 'lazy';
+        wrap.appendChild(iframe);
+        if (data.thumbnail_url) {
+            const thumb = document.createElement('img');
+            thumb.src = data.thumbnail_url;
+            thumb.alt = "Video thumbnail";
+            thumb.appendChild(thumb);
+        }
+    } else {
+        const p = document.createElement('p');
+        p.textContent = `unsupported media type ${data.media_type}`;
+        wrap.appendChild(p);
+    }
+    const meta = document.createElement('p');
+    meta.textContent = data.date || '';
+    h3.textContent = data.title;
+    wrap.appendChild(meta);
+    const desc = document.createElement('p');
+    desc.textContent = data.explanation || "no explanation";
+    wrap.appendChild(desc);
+    container.appendChild(wrap);
+}
+
+async function fetchPicture(params = {}) {
+    const resultContainer = document.querySelector('#resultContainer');
+
+    resultContainer.innerHTML = '';
+    const baseParams = {
+        api_key: API_KEY,
+        thumbs: true,
+    };
+    const qp = new URLSearchParams({...baseParams, ...params});
+
+    const isSingleDate = !!params.date;
+
+    const doFetch = async (searchParams) => {
+        const response = await fetch(`${BASE_URL}?${searchParams}`);
+        if (!response.ok) {throw response;}
+        return response.json();
+    }
+
+    try{
+        const data = await doFetch(qp);
+        if(isSingleDate){
+            displayResult(data, resultContainer);
+        } else {
+            for (let i = 0; i < data.length ; i++) {
+                displayResult(data[i], resultContainer);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function getOnePicture() {
+    let oneDate =  inputOneDate.value || currentDate;
+    fetchPicture({date: oneDate});
+}
+
+function getPicturesByDates() {
+    if(!inputFirstDate.value) {
+        inputFirstDate.style.border = '3px solid red';
+        return;
+    }
+    let firstDate =  inputFirstDate.value;
+    let secondDate =  inputSecondDate.value;
+    fetchPicture({start_date: firstDate, end_date: secondDate});
+}
+
+function getRandomPictures() {
+    let count = inputCount.value;
+    if(count < 1){ count = 1;}
+    fetchPicture({count: count});
+}
